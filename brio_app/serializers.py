5315dba178cde54a6c53ba8b6a276bbd80f59af6
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-04-15 15:17:25 -0400

    Cleaned up recipe syntax

--- brio_app/serializers.py
+++ brio_app/serializers.py
@@ -4,22 +4,23 @@ from .models import Workout, Exercise, Recipe, Ingredient
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        fields = ('id', 'name', 'recipe',)
+        fields = ('id', 'name', 'recipe')
 
 class RecipeSerializer(serializers.ModelSerializer):
     ingredients = IngredientSerializer(many=True, read_only=True)
     class Meta:
         model = Recipe
-        fields = ('id', 'name', 'image_url', 'calories', 'prep_time', 'directions', 'ingredients',)
+        fields = ('id', 'name', 'image_url', 'calories', 'prep_time', 'directions', 'ingredients')
 
 
 class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
-        fields = ('id', 'name', 'exercise_target', 'time', 'video_url', 'description', 'benefits', 'workout',)
+        fields = ('id', 'name', 'exercise_target', 'time', 'video_url', 'description', 'benefits', 'workout')
 
 
 class WorkoutSerializer(serializers.ModelSerializer):
+    exercises = ExerciseSerializer(many=True, read_only=True)
     class Meta:
         model = Workout
-        fields = ('id', 'name', 'image_url', 'target', 'workout_time',)
+        fields = ('id', 'name', 'image_url', 'target', 'workout_time')
