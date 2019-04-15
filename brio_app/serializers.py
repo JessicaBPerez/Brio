b326517504604cf99c9dca7a606b060e949a0f5e
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-04-15 14:34:18 -0400

    Created Model views

---
+++ brio_app/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Workout, Exercise, Recipe, Ingredient
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'recipe',)
+
+class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'image_url', 'calories', 'prep_time', 'directions', 'ingredients',)
+
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = ('id', 'name', 'exercise_target', 'time', 'video_url', 'description', 'benefits', 'workout',)
+
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+        fields = ('id', 'name', 'image_url', 'target', 'workout_time',)
--- brio_app/views.py
+++ brio_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import WorkoutSerializer, ExerciseSerializer, IngredientSerializer, RecipeSerializer
+from .models import Workout, Exercise, Recipe, Ingredient
 
 # Create your views here.
+class WorkoutView(viewsets.ModelViewSet):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+class ExerciseView(viewsets.ModelViewSet):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
+
+class RecipetView(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+class IngredientView(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
