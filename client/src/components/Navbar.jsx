39d1545b13d1ef861eddb8b520c133dcf552b85b
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-04-17 16:49:51 -0400

    Set up workout component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,18 +5,18 @@ class Navbar extends Component {
     render() {
         return (
             <div className="margin-bottom">
-                <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
-                    <div class="container"><a class="navbar-brand">Brio</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+                <nav className="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
+                    <div className="container"><a class="navbar-brand">Brio</a><button data-toggle="collapse" data-target="#navbarResponsive" className="navbar-toggler navbar-toggler-right" type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i className="fa fa-bars"></i></button>
                         <div
-                            class="collapse navbar-collapse" id="navbarResponsive">
-                            <ul class="nav navbar-nav ml-auto text-uppercase">
-                                <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="/">Home</a></li>
-                                <li class="nav-item" role="presentation"><Link to={`/workouts`} className="nav-link js-scroll-trigger">
+                            className="collapse navbar-collapse" id="navbarResponsive">
+                            <ul className="nav navbar-nav ml-auto text-uppercase">
+                                <li className="nav-item" role="presentation"><a className="nav-link js-scroll-trigger" href="/">Home</a></li>
+                                <li className="nav-item" role="presentation"><Link to={`/workouts`} className="nav-link js-scroll-trigger">
                                     workouts
                   </Link></li>
-                                <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#">About</a></li>
-                                <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#">Team</a></li>
-                                <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#">Contact</a></li>
+                                <li className="nav-item" role="presentation"><a className="nav-link js-scroll-trigger" href="#">About</a></li>
+                                <li className="nav-item" role="presentation"><a className="nav-link js-scroll-trigger" href="#">Team</a></li>
+                                <li className="nav-item" role="presentation"><a className="nav-link js-scroll-trigger" href="#">Contact</a></li>
                             </ul>
                         </div>
                     </div>
--- client/src/components/Workout.jsx
+++ client/src/components/Workout.jsx
@@ -3,6 +3,30 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 export default class Workout extends Component {
+    state = {
+        workouts: [],
+        newWorkout: {
+            name: '',
+            image_url: '',
+            target: '',
+            workout_time: ''
+        }
+    }
+
+    componentDidMount() {
+        this.fetchWorkouts()
+    }
+
+    fetchWorkouts = async () => {
+        try {
+            const response = await axios.get('/api/v1/workouts/')
+            this.setState({ workouts: response.data })
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
     render() {
         return (
             <div>
