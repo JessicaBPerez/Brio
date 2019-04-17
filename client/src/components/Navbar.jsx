7a24cf101f9f1583333d136fcd4ffd4879f3fafa
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-04-17 18:51:19 -0400

    Syncing databases

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ class Navbar extends Component {
         return (
             <div className="margin-bottom">
                 <nav className="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
-                    <div className="container"><a class="navbar-brand">Brio</a><button data-toggle="collapse" data-target="#navbarResponsive" className="navbar-toggler navbar-toggler-right" type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i className="fa fa-bars"></i></button>
+                    <div className="container"><a className="navbar-brand">Brio</a><button data-toggle="collapse" data-target="#navbarResponsive" className="navbar-toggler navbar-toggler-right" type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i className="fa fa-bars"></i></button>
                         <div
                             className="collapse navbar-collapse" id="navbarResponsive">
                             <ul className="nav navbar-nav ml-auto text-uppercase">
