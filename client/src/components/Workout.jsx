b851853988cadbff865b85ab1c910661a7493a50
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-04-17 20:50:33 -0400

    Created workout form

--- client/src/components/Workout.jsx
+++ client/src/components/Workout.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import WorkoutForm from './WorkoutForm.jsx'
 
 export default class Workout extends Component {
     state = {
@@ -28,9 +29,38 @@ export default class Workout extends Component {
     }
 
     render() {
+        if (this.state.error) {
+            return <div>{this.state.error}</div>
+        }
+
         return (
             <div>
-                <h1>Workout Component</h1>
+                <h1>All Workouts</h1>
+                {this.state.workouts.map(workout => {
+                    return (
+                        <div key={workout.id}>
+                            {/* <h3>{workout.name}</h3>
+                            <img src={workout.image_url} alt={workout.name} /> */}
+                            <Link to={`/workouts/${workout.id}/`}>{workout.name}</Link>
+                            <div class="card bg-dark text-white" style={{ maxWidth: '700px' }}>
+                                <img class="card-img" src={workout.image_url} alt={workout.name} />
+                                <div class="card-img-overlay">
+                                    <h5 class="card-title">{workout.name}</h5>
+                                    <p class="card-text"><Link to={`/workouts/${workout.id}/`}>{workout.name}</Link></p>
+                                    <p class="card-text">{workout.target}</p>
+                                    <p class="card-text">{workout.workout_time}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+                <h1>Create Workout</h1>
+                <WorkoutForm
+                    workout={this.state.newWorkout}
+                    handleChange={this.handleChange}
+                    handleSubmit={this.createWorkout}
+                    submitBtnText="Create"
+                />
             </div>
         )
     }
---
+++ client/src/components/WorkoutForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WorkoutForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Workout Form</h1>
+            </div>
+        )
+    }
+}
