66ca838942bdd6e4eaa6532e220f3635349fabc1
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-04-18 15:55:49 -0400

    Added Individual Exercise

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar.jsx'
 import Workout from './components/Workout';
 import IndividualWorkout from './components/IndividualWorkout';
 import Recipes from './components/Recipes.jsx'
+import IndividualExercise from './components/IndividualExercise.jsx'
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
               <Route exact path='/' component={Home} />
               <Route exact path='/workouts' component={Workout} />
               <Route path='/workouts/:id' component={IndividualWorkout} />
+              <Route exact path='/exercise/:id' component={IndividualExercise} />
               <Route exact path='/recipes' component={Recipes} />
             </Switch>
 
---
+++ client/src/components/IndividualExercise.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class IndividualExercise extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Individual Exercise</h1>
+            </div>
+        )
+    }
+}
